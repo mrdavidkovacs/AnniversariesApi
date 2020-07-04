1675f87b2c800b322d1d6caacdbd878c4235680d
David K <1780935+mrdavidkovacs@users.noreply.github.com>
2020-07-04 23:59:39 +0200

    Add more birthday reasons (#8)
    * Add new duration class
    
    * Add sorting
    
    * Add some more birthdays

--- Anniversaries.Core/BirthdayAnniversaries.cs
+++ Anniversaries.Core/BirthdayAnniversaries.cs
@@ -1,10 +1,13 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Anniversaries.Core
 {
     public class BirthdayAnniversaries : IAnniversaryRepository
     {
+        private const int Years = 150;
+
         public AnniversaryType GetAnniversaryType()
         {
             return new AnniversaryType("Geburtstag", "Geburtsdatum", "Caroline", AnniversaryTypes.Birthday, "fas fa-birthday-cake", new DateTime(2000, 01, 28));
@@ -12,15 +15,39 @@ namespace Anniversaries.Core
 
         public IEnumerable<Anniversary> GetAnniversaries()
         {
-            yield return new Anniversary("Geburtstag", 0, "Tag der Geburt");
+            yield return new Anniversary("Geburtstag", Duration.FromYears(0m), "Tag der Geburt");
 
-            for (int years = 10; years <= 1000; years += 5)
+            for (int years = 5; years <= Years; years++)
             {
-                if ((years < 100 && years % 10 == 0) || years % 25 == 0)
+                if (years % 5 == 0 || years.ToString().Distinct().Count() == 1)
                 {
-                    yield return new Anniversary($"{years}. Geburtstag", years, string.Empty);
+                     yield return new Anniversary($"{years}. Geburtstag", Duration.FromYears(years), string.Empty);
                 }
-            }            
+            }
+
+            for (int days = 1; days <= Years * 366; days++)
+            {
+                if (days % 1000 == 0 || days.ToString().Distinct().Count() == 1)
+                {
+                     yield return new Anniversary($"{days} Tage", Duration.FromDays(days), string.Empty);
+                }
+            }
+
+            for (int weeks = 1; weeks <= Years * 52; weeks++)
+            {
+                if (weeks % 1000 == 0 || weeks.ToString().Distinct().Count() == 1)
+                {
+                     yield return new Anniversary($"{weeks} Wochen", Duration.FromDays(weeks * 7), string.Empty);
+                }
+            }
+
+            for (int months = 1; months <= Years * 12; months++)
+            {
+                if (months % 100 == 0 || months.ToString().Distinct().Count() == 1)
+                {
+                     yield return new Anniversary($"{months} Monate", Duration.FromMonths(months), string.Empty);
+                }
+            }
         }
     }
 }
---
+++ Anniversaries.Core/Duration.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Anniversaries.Core
+{
+    public class Duration : IComparable<Duration>
+    {
+        private readonly decimal? _years;
+        private readonly int? _months;
+        private readonly int? _days;
+
+        private Duration(decimal? years, int? months, int? days)
+        {
+            if (!(years.HasValue ^ months.HasValue ^ days.HasValue))
+            {
+                throw new ArgumentException("You can only set one parameter.");
+            }
+
+            _years = years;
+            _months = months;
+            _days = days;
+        }
+
+        public DateTime CalculateConcreteDate(DateTime relevantDate)
+        {
+            if (_years.HasValue)
+            {
+                return relevantDate.AddMonths((int) (_years.Value * 12));
+            }
+
+            if (_months.HasValue)
+            {
+                return relevantDate.AddMonths(_months.Value);
+            }
+        
+            if (_days.HasValue)
+            {
+                return relevantDate.AddDays(_days.Value);
+            }
+
+            throw new InvalidOperationException("One of the values must be set!");
+        }
+
+        public int CompareTo([AllowNull] Duration other)
+        {
+            if (other == null)
+            {
+                return -1;
+            }
+
+            return this.GetDaysForComparision().CompareTo(other.GetDaysForComparision());            
+        }
+
+        public static Duration FromYears(decimal years)
+        {
+            return new Duration(years, null, null);
+        }
+
+        public static Duration FromMonths(int months)
+        {
+            return new Duration(null, months, null);
+        }
+
+        public static Duration FromDays(int days)
+        {
+            return new Duration(null, null, days);
+        }
+
+        private double GetDaysForComparision()
+        {
+            if (_years.HasValue)
+            {
+                return (int) (_years.Value * 365.2425m);
+            }
+
+            if (_months.HasValue)
+            {
+                return _months.Value * 30.43687d;
+            }
+        
+            if (_days.HasValue)
+            {
+                return _days.Value;
+            }
+
+            throw new InvalidOperationException("One of the values must be set!");
+        }
+    }
+}
