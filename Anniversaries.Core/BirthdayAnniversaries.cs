535f6a6617a518f5f0727dbb35844af5639a6b6f
David Kovacs <1780935+mrdavidkovacs@users.noreply.github.com>
2020-08-11 21:46:54 +0200

    Add binary birthday reasons

--- Anniversaries.Core/BirthdayAnniversaries.cs
+++ Anniversaries.Core/BirthdayAnniversaries.cs
@@ -7,6 +7,14 @@ namespace Anniversaries.Core
     public class BirthdayAnniversaries : IAnniversaryRepository
     {
         private const int Years = 150;
+        private readonly int[] _binaryNumbers;
+
+        public BirthdayAnniversaries()
+        {
+            _binaryNumbers = Enumerable.Range(1, 100)
+                .Select(nr => (int) Math.Pow(2, nr))
+                .ToArray();
+        }
 
         public AnniversaryType GetAnniversaryType()
         {
@@ -17,37 +25,80 @@ namespace Anniversaries.Core
         {
             yield return new Anniversary("Geburtstag", Duration.FromYears(0m), "Tag der Geburt");
 
-            for (int years = 5; years <= Years; years++)
+            for (int years = 1; years <= Years; years++)
             {
-                if (years % 5 == 0 || years.ToString().Distinct().Count() == 1)
+                var (relevant, reason) = this.GetAnniversaryReasons(years, 10);
+
+                if (!relevant)
                 {
-                     yield return new Anniversary($"{years}. Geburtstag", Duration.FromYears(years), string.Empty);
+                    continue;
                 }
+
+                yield return new Anniversary($"{years}. Geburtstag", Duration.FromYears(years), reason);
             }
 
             for (int days = 1; days <= Years * 366; days++)
             {
-                if (days % 1000 == 0 || days.ToString().Distinct().Count() == 1)
+                var (relevant, reason) = this.GetAnniversaryReasons(days, 1000);
+
+                if (!relevant)
                 {
-                     yield return new Anniversary($"{days} Tage", Duration.FromDays(days), string.Empty);
+                    continue;
                 }
+
+                yield return new Anniversary($"{days} Tage", Duration.FromDays(days), reason);
             }
 
             for (int weeks = 1; weeks <= Years * 52; weeks++)
             {
-                if (weeks % 1000 == 0 || weeks.ToString().Distinct().Count() == 1)
+                var (relevant, reason) = this.GetAnniversaryReasons(weeks, 1000);
+
+                if (!relevant)
                 {
-                     yield return new Anniversary($"{weeks} Wochen", Duration.FromDays(weeks * 7), string.Empty);
+                    continue;
                 }
+                
+                yield return new Anniversary($"{weeks} Wochen", Duration.FromDays(weeks * 7), reason);
             }
 
             for (int months = 1; months <= Years * 12; months++)
             {
-                if (months % 100 == 0 || months.ToString().Distinct().Count() == 1)
+                var (relevant, reason) = this.GetAnniversaryReasons(months, 100);
+
+                if (!relevant)
                 {
-                     yield return new Anniversary($"{months} Monate", Duration.FromMonths(months), string.Empty);
+                    continue;
                 }
+                
+                yield return new Anniversary($"{months} Monate", Duration.FromMonths(months), reason);
             }
+        }
+
+        private (bool, string) GetAnniversaryReasons(int number, int roundEveryX)
+        {
+            List<string> reasons = new List<string>();
+
+            if (number % roundEveryX == 0)
+            {
+                reasons.Add("Eine Runder.");
+            }
+
+            if (number.ToString().Distinct().Count() == 1)
+            {
+                reasons.Add("Nur eine Zahl in Verwendung.");
+            }
+
+            if (_binaryNumbers.Contains(number))
+            {
+                reasons.Add($"{number} ist eine Zweierpotenz.");
+            }
+
+            if (reasons.Any())
+            {
+                return (true, string.Join(" ", reasons));
+            }
+
+            return (false, string.Empty);
         }
     }
 }
